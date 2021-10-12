c38d976d32742f363bc50b2ece49db565538dd37
AvivYamin <aviv912@gmail.com>
2021-10-12 19:21:53 +0300

    I did the first task

---
+++ 03.callBackHell/firstTask.js
@@ -0,0 +1,15 @@
+function checkEqualSquare(num1, num2, num3){
+    if(squareValue(num1) + squareValue(num2) === squareValue(num3)){
+        return true;
+    }else{
+        return false;
+    }
+};
+
+function squareValue(num){
+    return multiplication(num, num);
+};
+
+function multiplication(num1, num2){
+    return num1 * num2;
+};
